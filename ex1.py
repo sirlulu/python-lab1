a1347a163a6ec1cc6e9a33f69179a734f3095e4f
sirlulu <s213076@studenti.polito.it>
2019-03-11 16:34:14 +0100

    Exercise 1

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#ex1
+
+while True:
+    num1 = int(input("Insert the first number: \n"))
+    num2 = int(input("Insert the second number: \n"))
+
+    print("The sum of %d and %d is %d" %(num1,num2,num1+num2))
+    if(input("Do you want to continue?\n")== 'no'):
+        break
